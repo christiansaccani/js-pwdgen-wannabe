96a9988b143e0d91a8fe997a02284112b3558dac
christiansaccani <christian.saccani99@gmail.com>
2024-01-25 14:19:15 +0100

    Exercise Done! (Console add)

--- js/script.js
+++ js/script.js
@@ -2,13 +2,19 @@ let firstname;
 
 firstname = prompt("Qual'è il tuo nome?");
 
+console.log(firstname);
+
 let surname;
 
 surname = prompt("Qual'è il tuo cognome?");
 
+console.log(surname);
+
 let favcolor;
 
 favcolor = prompt("Qual'è il tuo colore preferito?");
+
+console.log(favcolor);
 
 document.getElementById("password").innerHTML =
 ` ${firstname}${surname}${favcolor}.23 `;
